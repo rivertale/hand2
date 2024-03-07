4c22f61f319b5c554e8a79f61fb7c3a87e3a6c51
rivertale <fanghs666@gmail.com>
2024-03-07 17:43:05 +0800

    fix: user permission on docker

--- dependency_linux/hand_build.sh
+++ dependency_linux/hand_build.sh
@@ -3,7 +3,7 @@ image_name="hand2_linux_image"
 
 cd $(dirname $0)
 cd ..
-docker run --rm -v $(pwd):/hand ${image_name} sh -c "
+docker run --rm --user $(id -u):$(id -g) -v $(pwd):/hand ${image_name} sh -c "
     cd /hand/code
     ./build.sh
 "
